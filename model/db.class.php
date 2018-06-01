7deffe236488c28530e26fe83eb84f65d7d070b6
Ana Parac <anparac@pr1-15.studenti.math.hr>
2018-06-01 12:31:35 +0200

    Promijenjeni korisnički podaci.

--- model/db.class.php
+++ model/db.class.php
@@ -15,7 +15,7 @@ class DB
 	    	try
 	    	{
 	    		// Unesi ispravni HOSTNAME, DATABASE, USERNAME i PASSWORD
-		    	DB::$db = new PDO( "mysql: host=localhost; dbname=puzar; charset=utf8", 'root', 'sarasanja1' );
+		    	DB::$db = new PDO( "mysql: host=rp2.studenti.math.hr; dbname=parac; charset=utf8", 'student', 'pass.mysql' );
 		    	DB::$db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		    }
 		    catch( PDOException $e ) { exit( 'PDO Error: ' . $e->getMessage() ); }
