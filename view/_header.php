565a190896a898f5110081d8a2e93d2c21060e37
Sarapuz <sapuzar@student.math.hr>
2018-06-06 13:53:19 +0200

    Promijenjena skripta u heeaderu

--- view/_header.php
+++ view/_header.php
@@ -6,6 +6,9 @@
       <title></title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
       <link rel="stylesheet" href="style.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
       <style>
         .title{
           background-color:  #202020;
