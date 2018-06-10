c870c3b600426e2b602f59deb4c32d1c6d11cfcd
ana_parac <anaparac8@gmail.com>
2018-06-10 03:42:47 +0200

    Dodana klasa 'troškovi'.

---
+++ model/transaction.class.php
@@ -0,0 +1,33 @@
+<?php
+
+  /********************************
+   * Sve transakcije korisnika
+*/
+  class Transaction{
+    protected $tr_id, $category_name, $user_id,
+        $tr_name, $tr_value, $tr_date,  $tr_description, $tr_type;
+
+    function __construct($tr_id, $category_name, $user_id,
+        $tr_name, $tr_value, $tr_date,  $tr_description, $tr_type) {
+
+      $this->tr_id = $tr_id;
+      $this->category_name = $category_name;
+      $this->user_id = $user_id;
+      $this->tr_name = $tr_name;
+      $this->tr_value = $tr_value;
+      $this->tr_date = $tr_date;
+      $this->tr_description = $tr_description;
+      $this->tr_type = $tr_type;
+    }
+
+    function __get( $variable ){
+      return $this->$variable;
+    }
+
+    function __set( $variable, $value ){
+      $this->$variable = $value;
+      return $this;
+    }
+  };
+
+ ?>
