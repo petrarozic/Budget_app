5ec61c9fb8ec497510207e2f0d8dab4b5112ddb7
RPetra <perozic@student.math.hr>
2018-06-14 11:22:26 +0200

    Preoblikovan header

--- view/_header.php
+++ view/_header.php
@@ -9,13 +9,14 @@
           integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
           crossorigin="anonymous">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     </head>
 
-  <body>
+  <body class="body_">
     <div class="title">
-      <h1>Budget-app</h1>
+      <span class="title-name">Budget-app</span>
       <form class="logout" action="<?php echo __SITE_URL; ?>/index.php?rt=login" method="post">
         <input type="hidden" name="logout">
         <button type="submit" class="btn btn-outline-light" style="float:right; margin-top:25px;">logout</button>
