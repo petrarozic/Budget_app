098d83887977a4d2dcfaae88ad48efa1085dad1a
RPetra <perozic@student.math.hr>
2018-06-28 09:52:53 +0200

    Kreiran footer

--- view/_footer.php
+++ view/_footer.php
@@ -1,4 +1,47 @@
-    </div> <!--kraj sredisnjed dijela -->
-   </div>
+      </div> <!--End of main part -->
+    </div> <!--End of row -->
+    <!--FOOTER -->
+    <footer class="page-footer font-small mdb-color pt-4 mt-4">
+      <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left mt-3 pb-3">
+
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Budget-app</h6>
+            <p>Budget-app is an application for tracking personal finances.
+              <br> Spend smart and enjoy the app!
+            </p>
+          </div>
+
+          <hr class="w-100 clearfix d-md-none">
+
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Terms of use</h6>
+            <h6 class="text-uppercase mb-4 font-weight-bold">Privacy policy</h6>
+          </div>
+
+          <hr class="w-100 clearfix d-md-none">
+
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">CREATED BY</h6>
+            <p>Ana Parać, Sara Pužar</p>
+            <p>Petra Rožić, Paula Vujasić</p>
+            <hr>
+            <h6 class="text-uppercase mb-4 font-weight-bold">MENTOR</h6>
+            <p>Zvonimir Bujanović</p>
+          </div>
+
+          <hr class="w-100 clearfix d-md-none">
+
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+            <p><i class="fa fa-envelope mr-3"></i> info@gmail.com</p>
+            <hr>
+            <h6 class="text-uppercase mb-4 font-weight-bold">Follow</h6>
+            <p> IKONA OD fb / link na fb??</p>
+          </div>
+
+        </div><!-- Row Links -->
+      </div><!-- Footer Links -->
+    </footer><!-- Footer -->
   </body>
 </html>
