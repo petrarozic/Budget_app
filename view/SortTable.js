cbf4a5eeb7bc668c40fa70b3f3046673d59b58ca
RPetra <perozic@student.math.hr>
2018-07-06 13:51:58 +0200

    Sort za AMOUNT

--- view/SortTable.js
+++ view/SortTable.js
@@ -4,22 +4,18 @@ $( document ).ready( function()
   } );
 
 function sortTable(event) {
-  console.log("Sortiraj prema: " , event.currentTarget.innerText);
+  //console.log("Sortiraj prema: " , event.currentTarget.innerText);
   var sortIndex = event.currentTarget.cellIndex;
-  console.log("Celija = " , sortIndex);
+  //console.log("Celija = " , sortIndex);
 
   var brRedaka = $('table tbody').children().length;
   //console.log("Br. podataka = ", brRedaka);
 
-  //izvucem svu dijecu u listu -- sortiram tu listu
+  //IDEJA --> izvucem svu dijecu u listu -- sortiram tu listu
   var listChildren = [];
   for (var i = 0; i < brRedaka; i++) {
     listChildren.push($('table tbody').children().eq(i));
   }
-  //console.log(listChildren);
-
-  //isprazni tijelo tablice
-  $('table tbody').children().remove();
 
   //SORT ALGORITHM
   var swap = false;
@@ -30,6 +26,13 @@ function sortTable(event) {
       var dijete_next_j = listChildren[j+1];
       var value_next_j = dijete_next_j.children().eq(Number(sortIndex)).html();
       //console.log("VRIJEDNOSTI: j = ", value_j, "   next_j = ", value_next_j);
+
+      //ako sortiramo po AMOUNT onda sortiramo Number (ne String)
+      if (event.currentTarget.innerText == "AMOUNT (HRK)") {
+        value_j= Number(value_j);
+        value_next_j = Number (value_next_j);
+      }
+
       if (value_j > value_next_j ) {
         //ZAMJENA
         swap = true;
@@ -41,10 +44,11 @@ function sortTable(event) {
   }
   //end SORT ALGORITHM
 
-  //dodaj dijecu u tijelo tablice
-  // if (swap) console.log("BILO JE IZMJENA");
-  //  else console.log("NIJE BILO IZMJENA");
+  //isprazni tijelo tablice
+  $('table tbody').children().remove();
 
+  //dodaj dijecu u tijelo tablice
+  //ako nije bilo promjene(swap==false) onda vjv trazi suprotan sort (ASC/DECS)
   if (swap){
     for (var i = 0; i < brRedaka; i++)
       $('table tbody').append(listChildren[i])
