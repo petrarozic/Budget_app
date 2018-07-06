4fa427dab1a5e4713b90bc3ab7c3f6a0a13cbfe4
Sarapuz <sapuzar@student.math.hr>
2018-07-06 19:12:52 +0200

    Izbrisane funkcije vezane uz kategorije.

--- controller/transactionsController.php
+++ controller/transactionsController.php
@@ -78,55 +78,6 @@ function sendErrorAndExit( $messageText )
       $this->registry->template->show('transactions_index');
     }
 
-    function CategoryForSelect(){
-
-      $ls = new BudgetService();
-
-      if ( isset( $_GET['tip']) ){
-        $tip = $_GET['tip'];
-
-      $message = $ls->getCategoriesById($_SESSION['user_id'], $tip);
-
-
-      sendJSONandExit( $message );
-      }
-    }
-
-    function addCategory(){
-      $ls = new BudgetService();
-      $user_id = $_SESSION['user_id'];
-      $name = $_POST['name'];
-
-      if( !preg_match( '/^[A-Za-z0-9_@\\-\\.\\, ]{1,20}$/' , $name )){
-        $this->registry->template->message = "Name of category must consist of at most 20 letters or numbers." ;
-        $_SESSION['flag'] = 0;
-      }
-      else if( !isset($_POST['type']) ){
-        $this->registry->template->message = "Please, chose type of transaction." ;
-        $_SESSION['flag'] = 0;
-      }
-      else {
-        $type = $_POST['type'];
-        $this->registry->template->addingTransaction = $ls->addCategory($user_id, $type, $name);
-      }
-
-      if ( $_POST['SubmitButton'] == "expense" ){
-        $this->registry->template->transactionsList = $ls->getExpensesById( $user_id );
-        $this->registry->template->flag = "expense";
-      }
-      else if ( $_POST['SubmitButton'] == "income" ){
-        $this->registry->template->transactionsList = $ls->getIncomesById( $user_id );
-        $this->registry->template->flag = "income";
-      }
-      else if ( $_POST['SubmitButton'] == "transactions" ){
-        $this->registry->template->transactionsList = $ls->getTransactionsById( $user_id );
-        $this->registry->template->flag = "transactions";
-      }
-
-    //header( 'Location: ' . __SITE_URL . '/index.php?rt=home');
-      $this->registry->template->show('transactions_index');
-
-    }
 
     function addTransaction(){
 
@@ -140,7 +91,6 @@ function sendErrorAndExit( $messageText )
       $repeating = $_POST['repeating'];
 
       // Validating data : name, description ( other are specified with form )
-
 
       if( !preg_match( '/^[A-Za-z0-9_@\\-\\.\\, ]{1,20}$/' , $name )){
         $this->registry->template->message = "Name of transaction must consist of at most 20 letters or numbers." ;
@@ -181,7 +131,6 @@ function sendErrorAndExit( $messageText )
         $this->registry->template->flag = "transactions";
       }
 
-      //header( 'Location: ' . __SITE_URL . '/index.php?rt=home');
       $this->registry->template->show('transactions_index');
     }
 
