0f732a8757fdfdf495b3f6e0e1a8ceeae16edd85
Sarapuz <sapuzar@student.math.hr>
2018-07-07 14:42:35 +0200

    Notify poruke na hrvatskom jeziku.

--- controller/loginController.php
+++ controller/loginController.php
@@ -31,9 +31,16 @@ class LoginController extends BaseController
 			$id = $ls->isInDB( $_POST['username'], $_POST['password']);
 			if($id === false){
 				$this->registry->template->l_flag = 1;
-				$this->registry->template->lmessage = "Username or password incorrect.";
-				$_SESSION['flag'] = 0;
-				$this->registry->template->show( 'login_index' );
+				if(  !isset($_SESSION['lang']) || $_SESSION['lang'] == 'ENG' ){
+					$this->registry->template->lmessage = "Username or password incorrect.";
+					$_SESSION['flag'] = 0;
+					$this->registry->template->show( 'login_index' );
+				}
+				else if ( $_SESSION['lang'] == 'CRO' ){
+					$this->registry->template->lmessage = "Korisničko ime ili lozinka su pogrešni.";
+					$_SESSION['flag'] = 0;
+					$this->registry->template->show( 'login_indexCRO' );
+				}
 				exit();
 			}
 
@@ -46,9 +53,17 @@ class LoginController extends BaseController
 		}
 		else{
 			$this->registry->template->l_flag = 1;
-			$this->registry->template->lmessage = "You have to enter username and password to log in.";
-			$_SESSION['flag'] = 0;
-			$this->registry->template->show( 'login_index' );
+
+			if(  !isset($_SESSION['lang']) || $_SESSION['lang'] == 'ENG' ){
+				$this->registry->template->lmessage = "You have to enter username and password to log in.";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_index' );
+			}
+			else if ( $_SESSION['lang'] == 'CRO' ){
+				$this->registry->template->lmessage = "Molim Vas, upišite i korisničko ime i lozinku.";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_indexCRO' );
+			}
 			exit();
 		}
 	}
@@ -60,33 +75,62 @@ class LoginController extends BaseController
 		$this->registry->template->lmessage = '';
 		if( !isset( $_POST['username'] ) || !isset( $_POST['password'] ) || !isset( $_POST['email'] ) || $_POST['username'] === '' || $_POST['password'] === '' || $_POST['email'] === '')
 		{
-			$this->registry->template->smessage = "You have to enter username, password and email to sign up.";
-			$_SESSION['flag'] = 0;
-			$this->registry->template->show( 'login_index' );
+
+			if(  !isset($_SESSION['lang']) || $_SESSION['lang'] == 'ENG' ){
+				$this->registry->template->smessage = "You have to enter username, password and email to sign up.";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_index' );
+			}
+			else if ( $_SESSION['lang'] == 'CRO' ){
+				$this->registry->template->smessage = "Molim Vas, upišite, korisničko ime, lozinku i email adresu kako biste se registrirali.";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_indexCRO' );
+			}
 			exit();
 		}
 
 		else if( !preg_match( '/^[A-Za-z0-9_@ ]{3,20}$/', $_POST['username'] ) )
 		{
-			$this->registry->template->smessage = "Username should consist of letters and numbers.";
-			$_SESSION['flag'] = 0;
-			$this->registry->template->show( 'login_index' );
+			if(  !isset($_SESSION['lang']) || $_SESSION['lang'] == 'ENG' ){
+				$this->registry->template->smessage = "Username should consist of letters and numbers.";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_index' );
+			}
+			else if ( $_SESSION['lang'] == 'CRO' ){
+				$this->registry->template->smessage = "Korisničko ime se mora sastojati samo od slova i brojeva.";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_indexCRO' );
+			}
 			exit();
 		}
 
 		else if( !preg_match( '/^[A-Za-z0-9]{3,20}$/', $_POST['password'] ) )
 		{
-			$this->registry->template->smessage = "Password should consist only of letters and numbers and be 3-20 characters long.";
-			$_SESSION['flag'] = 0;
-			$this->registry->template->show( 'login_index' );
+			if(  !isset($_SESSION['lang']) || $_SESSION['lang'] == 'ENG' ){
+				$this->registry->template->smessage = "Password should consist only of letters and numbers and be 3-20 characters long.";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_index' );
+			}
+			else if ( $_SESSION['lang'] == 'CRO' ){
+				$this->registry->template->smessage = "Lozinka mora biti dugačka 3-20 znakova, te se mora sastojati samo od slova i brojeva.";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_indexCRO' );
+			}
 			exit();
 		}
 
 		else if( !filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL) )
 		{
-			$this->registry->template->smessage = "Email is not valid.";
-			$_SESSION['flag'] = 0;
-			$this->registry->template->show( 'login_index' );
+			if(  !isset($_SESSION['lang']) || $_SESSION['lang'] == 'ENG' ){
+				$this->registry->template->smessage = "Email is not valid..";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_index' );
+			}
+			else if ( $_SESSION['lang'] == 'CRO' ){
+				$this->registry->template->smessage = "Unesena email adresa nije valjana.";
+				$_SESSION['flag'] = 0;
+				$this->registry->template->show( 'login_indexCRO' );
+			}
 			exit();
 		}
 		else
@@ -94,9 +138,16 @@ class LoginController extends BaseController
 			$is = $ls->isAlreadyInDB( $_POST['username']);
 			if($is === true)
 			{
-				$this->registry->template->smessage = "There is already a user with username ". $_POST['username'] .". Please, choose another username or check if you have already registrated.";
-				$_SESSION['flag'] = 0;
-				$this->registry->template->show( 'login_index' );
+				if(  !isset($_SESSION['lang']) || $_SESSION['lang'] == 'ENG' ){
+					$this->registry->template->smessage = "There is already a user with username ". $_POST['username'] .". Please, choose another username or check if you have already registrated.";
+					$_SESSION['flag'] = 0;
+					$this->registry->template->show( 'login_index' );
+				}
+				else if ( $_SESSION['lang'] == 'CRO' ){
+					$this->registry->template->smessage = "Već postoji korisnik s unesenim imenom ". $_POST['username'] .". Molim Vas odaberite drugo ime ili provjerite jeste je li već registrirani.";
+					$_SESSION['flag'] = 0;
+					$this->registry->template->show( 'login_indexCRO' );
+				}
 				exit();
 			}
 
@@ -129,8 +180,14 @@ class LoginController extends BaseController
 		exit( 'Something is wrong with the sequence.' );
 
 		$ls->findWithSequence( $_GET['sequence']);
-		$this->registry->template->output = "Registration has been successfully completed, you can now log in.";
-		$this->registry->template->show( 'login_index' );
+		if(  !isset($_SESSION['lang']) || $_SESSION['lang'] == 'ENG' ){
+			$this->registry->template->smessage = "Registration has been successfully completed, you can now log in.";
+			$this->registry->template->show( 'login_index' );
+		}
+		else if ( $_SESSION['lang'] == 'CRO' ){
+			$this->registry->template->smessage = "Registracija je bila uspiješna, sada se možete prijaviti.";
+			$this->registry->template->show( 'login_indexCRO' );
+		}
 	}
 };
 
